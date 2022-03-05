4bb6226e8ff27e0e82148cb9674dfece7987ca9f
younlll <youn.lee96@gmail.com>
2022-03-05 22:14:39 +0900

    init commit

---
+++ src/main/java/com/youn/realworldapp/RealWorldAppApplication.java
@@ -0,0 +1,13 @@
+package com.youn.realworldapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealWorldAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youn/realworldapp/RealWorldAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youn.realworldapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
